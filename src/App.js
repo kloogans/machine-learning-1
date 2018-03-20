27e08567e9a5dca43cd38d9561cc6aaad3ec85dc
notjamesobrien <jtwebco@gmail.com>
2018-03-19 23:16:38 -0400

    randomly generate colors on inital mount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import brain from 'brain.js'
+import randomColor from 'randomcolor'
 import './App.css'
+
+// Initialize a new neural network
 const network = new brain.NeuralNetwork()
 
 class App extends Component {
@@ -10,13 +13,23 @@ class App extends Component {
   }
 
   componentDidMount () {
+
     network.train([
       { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
       { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { light: 1 } },
       { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
       { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
-      { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } }
+      { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
+      { input: { r: 0.23, g: 0.27, b: 0.91 }, output: { light: 1 } }
     ])
+
+    const rgb = this.hexToRgb(randomColor())
+    const result = brain.likely(rgb, network)
+    const fontColor = result === 'dark' ? '#f7f7f7' : '#333333'
+    this.setState({
+      backgroundColor: randomColor(),
+      fontColor: fontColor
+    })
   }
 
   hexToRgb = hex => {
@@ -32,13 +45,13 @@ class App extends Component {
       const hex = e.target.value
       const rgb = this.hexToRgb(hex)
       const result = brain.likely(rgb, network)
-      console.log(result)
       const fontColor = result === 'dark' ? '#f7f7f7' : '#333333'
       this.setState({
         backgroundColor: hex,
         fontColor: fontColor
       })
     }
+
   render() {
     const containerStyle = {
       backgroundColor: this.state.backgroundColor
@@ -48,8 +61,10 @@ class App extends Component {
     }
     return (
       <div className='container' id='main-container' style={containerStyle}>
-        <h1 className='heading' id='heading' style={fontStyle}>HALLO</h1>
-        <input type='color' id='color-input' onChange={this.changeColorData} className='app__input--color' value='#5b2dc7' />
+        <h1 className='heading' id='heading' style={fontStyle}>
+          Font color will change based on color lightness detected via machine learning.
+        </h1>
+        <input ref='colorInput' type='color' id='color-input' onChange={this.changeColorData} className='app__input--color' defaultValue={this.state.backgroundColor} />
       </div>
     )
   }
